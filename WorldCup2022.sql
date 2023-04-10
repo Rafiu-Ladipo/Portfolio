abad0e6172b32275c30b97f34bf6d6484c16c538
Rafiu Olawale Ladipo <122757446+Rafiu-Ladipo@users.noreply.github.com>
2023-04-10 15:06:03 +0100

    Add files via upload

---
+++ WorldCup2022.sql
@@ -0,0 +1,35 @@
+use worldcup;
+show tables;
+select * from worldcup2022;
+select position, team, win, points, from worldcup2022 limit 2;
+select position, team, win, points from worldcup2022 order by points desc;
+select team from worldcup2022 as semifinalist limit 4;
+
+/* alter table */
+alter table worldcup2022 
+add column rating varchar (20);
+
+update worldcup2022
+set rating = "Champion"
+where Team = "Argentina";
+
+ update worldcup2022
+ set rating = "Runner-Up"
+ where Team = "France";
+ 
+ update worldcup2022
+ set rating = "Thirdplace"
+ where Team = "Croatia";
+ 
+ update worldcup2022
+ set rating = "Others"
+ where Team not in ("Argentina", "France", "Croatia");
+ 
+ set sql_safe_updates = 0;
+ select * from worldcup2022;
+ 
+ Select * from worldcup2022 where team = "France";
+ 
+ /* Wild Card*/
+ Select * from worldcup2022 where team like 'P%';
+ 
